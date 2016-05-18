6c8bcbc0c851b5643f023b48aa549f4ead053ba0
Kevin Lint <kevinlint@toolhouse.com>
2016-05-18 16:58:25 -0700

    add crud scaffold

---
+++ src/core/Oem.js
@@ -0,0 +1,38 @@
+oem = (function(Oem) {
+
+    /**
+     * Initialize components
+     */
+    Oem.init = function(){};
+
+    /**
+     * Create component
+     */
+    Oem.create = function(component, settings){};
+
+    /**
+     * Read component
+     *
+     * @param      {<type>}  component  The component
+     */
+    Oem.read = function(component){};
+
+    /**
+     * Update component
+     *
+     * @param      {<type>}  component  The component
+     * @param      {<type>}  settings   The settings
+     */
+    Oem.update = function(component, settings){};
+
+    /**
+     * Delete component
+     */
+    Oem.delete = function(component){};
+
+    /**
+     * Return main oem namespace object
+     */
+    return Oem;
+    
+})(oem);
