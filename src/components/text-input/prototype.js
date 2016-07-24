493d66102d4ba5326187bd8e2e8e3de04decfc79
kvnlnt <kvnlnt@yahoo.com>
2016-07-23 19:57:02 -0700

    refactor out field mixing

--- src/components/text-input/prototype.js
+++ src/components/text-input/prototype.js
@@ -10,62 +10,7 @@ oem.Components = (function(Components, Core) {
     Core.Util.mixin(Prototype, Core.Field); // mixin field functions
 
     Prototype.init = function(){
-        var input = this.getEl(); 
-        this.label = this.getLabel();
-        this.field = this.getField();
-        this.fieldName = this.getFieldName();
-        this.errors = this.getErrors();
-        this.hasValidatedOnce = true; // errors are displayed and interactively persist only AFTER a validation has taken place
-        this.field.addEventListener('input', this.handleInputChange.bind(this)); // get the input field
-    };
-
-    // GETTERS
-
-    Prototype.getLabel = function(){
-        return this.getEl().querySelector('label').innerText;
-    };
-
-    Prototype.getField = function(){
-        return this.getEl().querySelector('input[type="text"]');
-    };
-
-    Prototype.getFieldName = function(){
-        return this.field.getAttribute('name');
-    };
-
-    Prototype.getErrors = function(){
-        return this.getEl().querySelector('ul.errors');
-    };
-
-    // SETTERS
-
-    Prototype.setLabel = function(label){
-        this.getEl().querySelector('label').innerText = label;
-        this.label = label;
-        return this;
-    };
-
-    Prototype.setField = function(field){
-        this.field = field;
-        return this;
-    };
-
-    Prototype.setFieldName = function(fieldName){
-        this.field.setAttribute('name', fieldName);
-        this.fieldName = fieldName;
-        return this;
-    };
-
-    Prototype.setErrors = function(liErrors){
-        this.getEl().querySelector('ul.errors').innerHTML = liErrors;
-        this.errors = this.getEl().querySelector('ul.errors');
-        return this;
-    };
-
-    // METHODS
-
-    Prototype.handleInputChange = function(){
-        this.validate();
+        this.setupInputField();
     };
 
     Components.TextInput.Prototype = Prototype;
--- src/core/Field.js
+++ src/core/Field.js
@@ -10,6 +10,64 @@ oem.Core = (function(Core) {
 
     var Field = {};
 
+     // GETTERS
+
+    Field.getLabel = function(){
+        return this.getEl().querySelector('label').innerText;
+    };
+
+    Field.getField = function(){
+        return this.getEl().querySelector('input[type="text"]');
+    };
+
+    Field.getFieldName = function(){
+        return this.field.getAttribute('name');
+    };
+
+    Field.getErrors = function(){
+        return this.getEl().querySelector('ul.errors');
+    };
+
+    // SETTERS
+
+    Field.setLabel = function(label){
+        this.getEl().querySelector('label').innerText = label;
+        this.label = label;
+        return this;
+    };
+
+    Field.setField = function(field){
+        this.field = field;
+        return this;
+    };
+
+    Field.setFieldName = function(fieldName){
+        this.field.setAttribute('name', fieldName);
+        this.fieldName = fieldName;
+        return this;
+    };
+
+    Field.setErrors = function(liErrors){
+        this.getEl().querySelector('ul.errors').innerHTML = liErrors;
+        this.errors = this.getEl().querySelector('ul.errors');
+        return this;
+    };
+
+    // METHODS
+    
+    Field.setupInputField = function(){
+        this.label = this.getLabel();
+        this.field = this.getField();
+        this.fieldName = this.getFieldName();
+        this.errors = this.getErrors();
+        this.validateOnChange = false; // errors are displayed and interactively persist only AFTER a validation has taken place
+        this.field.addEventListener('input', this.handleInputChange.bind(this)); // get the input field
+    };
+
+    Field.handleInputChange = function(){
+        this.validate();
+    };
+
     Field.validate = function(){
 
         // create a brand new one each time, that's right start over
@@ -54,7 +112,7 @@ oem.Core = (function(Core) {
         if(validator.isValid) return false;
 
         // don't do anything until we've validated things
-        if(!this.hasValidatedOnce) return false;
+        if(!this.validateOnChange) return false;
 
         var errors = validator.errors[this.fieldName];
 
