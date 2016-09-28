799b2713fe14a241f836c90d23805b36167cc58f
Kevin Lint <kevinlint@toolhouse.com>
2016-09-28 15:07:46 -0700

    factory reset

--- cli/options/factory-reset.js
+++ cli/options/factory-reset.js
@@ -1,15 +1,30 @@
 const pkg = require('../../package');
 const chalk = require('chalk');
+const fs = require('fs-extra');
 
 /**
  * Component Creator
  */
 const FactoryReset = function(componentName){
+    this.componentsDirectory = './src/components;
+    this.configurationsDirectory = './src/configurations';
+    this.customizations = "./src/customizations";
     this.reset();
 };
 
 FactoryReset.prototype = {
     reset: function(){
+
+        fs.removeSync(this.componentsDirectory);
+        fs.mkdirsSync(this.componentsDirectory);
+        fs.removeSync(this.configurationsDirectory);
+        fs.mkdirsSync(this.configurationsDirectory);
+        fs.removeSync(this.customizations);
+        fs.mkdirsSync(this.customizations);
+        pkg.oem.deployment = {};
+        pkg.oem.development = {};
+        pkg.oem.components = {};
+        fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 4));
 
         console.log("");
         console.log("");
@@ -18,7 +33,7 @@ FactoryReset.prototype = {
         console.log(chalk.bgWhite("       "));
         console.log("");
         console.log("");
-        console.log("...coming soon");
+        console.log("You now have no components, no configurations & no deployments.");
         console.log("");
         console.log("");
 
