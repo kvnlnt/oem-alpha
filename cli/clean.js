5d9924e98cd47971b8dda784b9f4242ec74f2fc8
Kevin Lint <kevinlint@toolhouse.com>
2016-11-01 08:17:14 -0700

    add clean option

---
+++ cli/clean.js
@@ -0,0 +1,34 @@
+const pkg = require('../package');
+const chalk = require('chalk');
+const fs = require('fs-extra');
+
+const Clean = function(componentName){
+    this.clean().reply();
+};
+
+Clean.prototype = {
+
+    clean: function(){
+        fs.removeSync('./deploy');
+        fs.removeSync('./demo');
+        return this;
+    },
+
+    reply: function(){
+        console.log("");
+        console.log("");
+        console.log(chalk.bgWhite("       "));
+        console.log(chalk.black.bgWhite("  OEM  "), " CLEAN ");
+        console.log(chalk.bgWhite("       "));
+        console.log("");
+        console.log("");
+        console.log('Files cleaned');
+        console.log("");
+        console.log("");
+    }
+
+};
+
+module.exports = {
+    Clean: Clean
+};
--- oem.js
+++ oem.js
@@ -5,19 +5,21 @@
  *
  */
 
-const fs = require("fs");
-const pkg = require('./package');
-const exec = require('child_process').exec;
+const ARGS = process.argv.filter(function(arg, i){ return i > 1; });
+const Clean = require("./cli/clean").Clean;
 const CreateComponent = require('./cli/create').CreateComponent;
 const Demo = require('./cli/demo').Demo;
 const Deployment = require('./cli/deploy').Deployment;
 const DevelopComponent = require('./cli/develop').DevelopComponent;
+const exec = require('child_process').exec;
+const fs = require("fs");
 const Help = require('./cli/help');
 const List = require('./cli/list').List;
+const pkg = require('./package');
 const RemoveComponent = require('./cli/remove').RemoveComponent;
-const ARGS = process.argv.filter(function(arg, i){ return i > 1; });
 
 const ARG = {};
+ARG.CLEAN = 'clean';
 ARG.CREATE = 'create';
 ARG.DEMO = 'demo';
 ARG.DEPLOY = 'deploy';
@@ -29,24 +31,12 @@ ARG.REMOVE = 'remove';
 // actions
 try {
     switch(ARGS[0]){
-        case ARG.LIST:
-            var list = new List();
-            break;
-        case ARG.DEVELOP:
-            if(ARGS[1] === void 0) throw 'please specify a component';
-            if(!pkg.oem.development.hasOwnProperty(ARGS[1])) throw 'no such component exists, check package.json';
-            var developComponent = new DevelopComponent(ARGS[1], ARGS);
-            break;
-        case ARG.HELP:
-            Help.show();
+        case ARG.CLEAN:
+            var clean = new Clean();
             break;
         case ARG.CREATE:
             if(pkg.oem.development.hasOwnProperty(ARGS[1])) throw 'component already exists';
             var newComponent = new CreateComponent(ARGS[1]);
-            break;
-        case ARG.REMOVE:
-            if(!pkg.oem.development.hasOwnProperty(ARGS[1])) throw 'component does not exist';
-            var removeComponent = new RemoveComponent(ARGS);
             break;
         case ARG.DEMO:
             if(ARGS[1] === void 0) throw 'please specify a demo configuration. See package.oem.demos';
@@ -57,6 +47,21 @@ try {
             if(ARGS[1] === void 0) throw 'please specify a deployment configuration. See package.oem.deployments';
             if(pkg.oem.deployments[ARGS[1]] === void 0) throw 'no such deployment exists, check package.json';
             var deployment = new Deployment(ARGS[1]);
+            break;
+        case ARG.DEVELOP:
+            if(ARGS[1] === void 0) throw 'please specify a component';
+            if(!pkg.oem.development.hasOwnProperty(ARGS[1])) throw 'no such component exists, check package.json';
+            var developComponent = new DevelopComponent(ARGS[1], ARGS);
+            break;
+        case ARG.HELP:
+            Help.show();
+            break;
+        case ARG.LIST:
+            var list = new List();
+            break;
+        case ARG.REMOVE:
+            if(!pkg.oem.development.hasOwnProperty(ARGS[1])) throw 'component does not exist';
+            var removeComponent = new RemoveComponent(ARGS);
             break;
         default:
             Help.show();
