4f0ddcffd62f4051c2e781e73c74182297415cd4
Kevin Lint <kevinlint@toolhouse.com>
2016-11-17 09:58:33 -0800

    add phantomjs back in to support travis ci

--- cli/test.js
+++ cli/test.js
@@ -2,16 +2,16 @@ const oem = require('../oem.json');
 const chalk = require('chalk');
 const fs = require('fs-extra');
 const Deployment = require('./deploy').Deployment;
-var webdriverio = require('webdriverio');
-var options = {
+const phantomjs = require('phantomjs-prebuilt');
+const webdriverio = require('webdriverio');
+const options = {
     desiredCapabilities: {
-        browserName: 'chrome',
-        ensureCleanSession: true
+        browserName: 'chrome'
     },
     host: 'localhost',
     port: 4444
 };
-var client = webdriverio.remote(options);
+const client = webdriverio.remote(options);
 
 const Test = function (deployment) {
     this.deployment = deployment;
@@ -29,49 +29,52 @@ Test.prototype = {
 
         var regex = /(<([^>]+)>)/ig;
 
-        client
-            .init()
-            .url('./deploy/' + this.deployment + '/index.html')
-            .waitForExist('html.tests-completed', 5000)
-            .then(function(res){
-              console.log('Testing complete:');
-              console.log("");
-            })
-            .catch(function(err){
-              console.log('Tests did not complete succesfully', err);
-              process.exit(1);
-            })
-            .getHTML('li.test')
-            .then(results => {
-                var isValid = true;
-                var testsPassed = 0;
-                var testsFailed = 0;
-                results.forEach(function (result) {
-                    var resultText = result.replace(regex, "");
-                    var passed = result.indexOf("test-pass") != -1;
-                    if (passed) {
-                        console.log(chalk.green('\u2713'), resultText);
-                        testsPassed += 1;
-                    } else {
-                        isValid = false;
-                        testsFailed += 1;
-                        console.log(chalk.red('\u2717'), resultText);
-                    }
-                });
-                console.log("");
-                console.log(chalk.green(testsPassed), "passed");
-                console.log(chalk.red(testsFailed), "failed");
-                if (!isValid) process.exit(1);
-                console.log("");
-                return;
-            }).
-            catch(function(err){
-                console.log('No tests found');
-                process.exit(1);
-            })
-            .end();
-
+        phantomjs.run('--webdriver=4444').then(program => {
+            client
+                .init()
+                .url('./deploy/' + this.deployment + '/index.html')
+                .waitForExist('html.tests-completed', 5000)
+                .then(function (res) {
+                    console.log('Testing complete:');
+                    console.log("");
+                })
+                .catch(function (err) {
+                    console.log('Tests did not complete succesfully', err);
+                    process.exit(1);
+                })
+                .getHTML('li.test')
+                .then(results => {
+                    var isValid = true;
+                    var testsPassed = 0;
+                    var testsFailed = 0;
+                    results.forEach(function (result) {
+                        var resultText = result.replace(regex, "");
+                        var passed = result.indexOf("test-pass") != -1;
+                        if (passed) {
+                            console.log(chalk.green('\u2713'), resultText);
+                            testsPassed += 1;
+                        } else {
+                            isValid = false;
+                            testsFailed += 1;
+                            console.log(chalk.red('\u2717'), resultText);
+                        }
+                    });
+                    console.log("");
+                    console.log(chalk.green(testsPassed), "passed");
+                    console.log(chalk.red(testsFailed), "failed");
+                    if (!isValid) process.exit(1);
+                    program.kill();
+                    console.log("");
+                    return;
+                })
+                .catch(function (err) {
+                    console.log('No tests found');
+                    process.exit(1);
+                })
+                .end();
+        });
         return this;
+
     },
 
     reply: function () {
