a9ffc79491f841001e345ac57ea3f7ced3d6c452
kvnlnt <kvnlnt@yahoo.com>
2017-01-15 19:33:40 -0800

    Adds close button to drawer

--- development/drawer/prototype.js
+++ development/drawer/prototype.js
@@ -1,25 +1,33 @@
-(function(COMPONENTS, COMPONENT, PROTOTYPE) {
+(function(COMPONENTS, COMPONENT, EL, PROTOTYPE) {
 
-
-    // PROTOTYPE
-    // ========================================================
-    // This is the main prototype class for this component. It is meant to:
-    // 1) contain any/all functional behavior for this component.
-    // 2) be prototyped into a new instance for each component
     var Prototype = PROTOTYPE(COMPONENT, {
         type: "Drawer"
     });
 
+    Prototype.CLOSE_BUTTON_MODE = {
+        ALWAYS: "--close-btn-always",
+        ONLY_FULLSCREEN: "--close-btn-only-fullscreen",
+        NEVER: "--close-btn-never"
+    };
+
     Prototype.init = function(){
         this._isOpen = false;
         this.fullScreenAt = parseInt(this.getEl().dataset.oemFullScreenAt) || 0;
+        this.closeBtn = this.createCloseBtn();
+        this.closeBtnMode = this.getEl().dataset.oemCloseButtonMode || 'ALWAYS';
+        this.getEl().classList.add(Prototype.CLOSE_BUTTON_MODE[this.closeBtnMode]);
 
+        // add button to dom
+        this.getEl().appendChild(this.closeBtn);
+
+        // register vents
         var events = {};
         events.opened = this.getId() + ":opened";
         events.closed = this.getId() + ":closed";
         this.setEvents(events);
 
         oem.events.addEventListener(oem.EVENTS.WINDOW_RESIZED, this.manageFullScreen.bind(this));
+        this.closeBtn.addEventListener("click", this.close.bind(this));
 
         return this;
     };
@@ -31,6 +39,11 @@
         return this;
     };
 
+    Prototype.createCloseBtn = function(){
+        var btn = EL("button", {"class":"__closeBtn"}, EL("span"));
+        return btn;
+    };
+
     Prototype.getFullScreenAt = function(){
         return this.fullScreenAt;
     };
@@ -38,9 +51,9 @@
     Prototype.manageFullScreen = function(){
         var isFullScreen = window.innerWidth <= this.getFullScreenAt();
         if(isFullScreen) {
-             this.getEl().classList.add('--fullscreen')
+             this.getEl().classList.add('--fullscreen');
         } else {
-             this.getEl().classList.remove('--fullscreen')
+             this.getEl().classList.remove('--fullscreen');
         }
         return this;
     };
@@ -75,4 +88,4 @@
     COMPONENTS.Drawer.Prototype = Prototype;
     return COMPONENTS;
 
-})(oem.Components, oem.Core.Component, oem.Core.Prototype);
+})(oem.Components, oem.Core.Component, oem.Core.El, oem.Core.Prototype);
