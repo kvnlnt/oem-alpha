9a226056d9fbec4387d8debb7c88ed13f2d1acd4
Kevin Lint <kevinlint@toolhouse.com>
2017-03-06 09:45:46 -0800

    WIP: templating

--- development/accordion/module.js
+++ development/accordion/module.js
@@ -3,14 +3,24 @@
     var Accordion = {};
 
     Accordion.Params = {
+        id: String,
         title: String,
-        items: Array.of({
-            item: String,
-            body: String
+        list: Array.of({
+            term: String,
+            definition: String,
+            expanded: Boolean
         })
     };
 
-    
+    Accordion.Template = '';
+    Accordion.Template += '<% this.title %>';
+    Accordion.Template += '<dl data-oem-id="<% this.id %>" data-oem="Accordion">';
+    Accordion.Template += '<%for(var item in this.list) {%>';
+    Accordion.Template += '<dt><% this.list[item].term %></dt>';
+    Accordion.Template += '<dd><% this.list[item].definition %></dd>';
+    Accordion.Template += '<%}%>';
+    Accordion.Template += '</dl>';
+
     // exports
     COMPONENTS.Accordion = Accordion;
     return COMPONENTS;
